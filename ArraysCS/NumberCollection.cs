44258e364272499e3ca15689aaa5e179ee7b095d
GET Academy <terje@kolderup.net>
2023-11-08 15:30:58 +0100

    Initial commit

---
+++ ArraysCS/NumberCollection.cs
@@ -0,0 +1,30 @@
+﻿namespace ArraysCS
+{
+    internal class NumberCollection
+    {
+        static int[] numbers = new int[5];
+
+        public static void Run()
+        {
+            var count = 0;
+            while (count < numbers.Length)
+            {
+                var number = MyConsole.AskForInt("Skriv inn et tall: ");
+                var index = count;
+                numbers[index] = number;
+                count++;
+                Show();
+            }
+        }
+
+        static void Show()
+        {
+            foreach (var number in numbers)
+            {
+                Console.Write(number + " ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
